6caec125cf1b9eef0eb8947a027ae366cb0a12ac
Eschechola <lucas.eschechola@outlook.com>
2021-07-09 03:02:46 -0300

    Add Main page

---
+++ src/pages/Main/main.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from "react";
+import './main.css';
+
+const Main = (): ReactElement => {
+    return(
+        <section className="main">
+            Main Page!
+        </section>
+    );
+};
+
+export default Main;
